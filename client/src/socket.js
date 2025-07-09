9b521df92b09a36c23e7b078c172a27f3e104da8
Benjamini Omary <167584894+basanzietech@users.noreply.github.com>
2025-07-10 00:39:54 +0300

    Update socket.js
    Nime toa port ya localhost kua hii https://live-broadcast-backed.vercel.app/

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5000');
+const socket = io('https://live-broadcast-backed.vercel.app');
 socket.on('connect', () => {
   console.log('Socket.io connected!', socket.id);
 });
