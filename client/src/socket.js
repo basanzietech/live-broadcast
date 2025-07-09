2520151cdcd8fd4b0d52c285f9751532785c3e08
Benjamini Omary <167584894+basanzietech@users.noreply.github.com>
2025-07-10 00:53:17 +0300

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io('https://live-broadcast-backed.vercel.app');
+const socket = io('https://live-broadcast-sb5d.onrender.com');
 socket.on('connect', () => {
   console.log('Socket.io connected!', socket.id);
 });
